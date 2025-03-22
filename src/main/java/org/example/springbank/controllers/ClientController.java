12304929f3d93544ae8a1f823d63391d46963294
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-03-22 22:17:39 +0100

    Added ClientController

---
+++ src/main/java/org/example/springbank/controllers/ClientController.java
@@ -0,0 +1,39 @@
+package org.example.springbank.controllers;
+
+import org.example.springbank.models.Client;
+import org.example.springbank.services.ClientService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class ClientController {
+
+    static final int DEFAULT_GROUP_ID = -1;
+    static final int ITEMS_PER_PAGE = 5;
+    private final ClientService clientService;
+
+    public ClientController(ClientService clientService){
+        this.clientService = clientService;
+    }
+
+    public String index(Model model,
+                        @RequestParam(required = false, defaultValue = "0") Integer page){
+        if(page < 0) page = 0;
+
+        List<Client> clients = clientService.findAll(PageRequest.of(page, ITEMS_PER_PAGE, Sort.Direction.DESC, "id"));
+
+        model.addAttribute("clients", clients);
+        model.addAttribute("allPages", getPageCount());
+        return "index";
+    }
+
+    private long getPageCount() {
+        long totalCount = clientService.count();
+        return (totalCount / ITEMS_PER_PAGE) + ((totalCount % ITEMS_PER_PAGE > 0) ? 1 : 0);
+    }
+}
