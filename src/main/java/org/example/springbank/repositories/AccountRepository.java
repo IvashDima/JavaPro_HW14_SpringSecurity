04f74400203605182d88f9662b59e40fdf06d730
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-03-23 22:52:14 +0100

    Added Queries in AccountRepository

--- src/main/java/org/example/springbank/repositories/AccountRepository.java
+++ src/main/java/org/example/springbank/repositories/AccountRepository.java
@@ -1,9 +1,24 @@
 package org.example.springbank.repositories;
 
 import org.example.springbank.models.Account;
+import org.example.springbank.models.Client;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    @Query("SELECT c FROM Account c WHERE LOWER(c.balance) LIKE LOWER(CONCAT('%', :pattern, '%'))")
+    List<Account> findByPattern(@Param("pattern") String pattern, Pageable pageable);
+
+    @Query("SELECT c FROM Account c WHERE c.client = :client")
+    List<Account> findByClient(@Param("client") Client client, Pageable pageable);
+
+    @Query("SELECT COUNT(c) FROM Account c WHERE c.client = :client")
+    long countByClient(@Param("client") Client client);
 }
