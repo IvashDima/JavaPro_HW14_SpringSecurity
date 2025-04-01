028c66183eeacd8aba156c8a0671e86c754af75c
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-04-01 21:42:21 +0200

    added user service

---
+++ src/main/java/org/example/springbank/services/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package org.example.springbank.services;
+
+import org.example.springbank.models.CustomUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserService userService;
+
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        CustomUser customUser = userService.findByLogin(login);
+        if (customUser == null)
+            throw new UsernameNotFoundException(login + " not found");
+
+        List<GrantedAuthority> roles = Arrays.asList(
+                new SimpleGrantedAuthority(customUser.getRole().toString())
+        );
+
+        return new User(customUser.getLogin(), customUser.getPassword(), roles);
+    }
+}
