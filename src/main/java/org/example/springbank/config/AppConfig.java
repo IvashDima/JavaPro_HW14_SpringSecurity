181c8a4cebde7006f6e939e1288e6bbed06a4fca
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-04-02 23:29:48 +0200

    fixed autorization bug.

--- src/main/java/org/example/springbank/config/AppConfig.java
+++ src/main/java/org/example/springbank/config/AppConfig.java
@@ -1,19 +1,22 @@
 package org.example.springbank.config;
 
-import org.example.springbank.enums.CurrencyType;
-import org.example.springbank.models.Account;
-import org.example.springbank.models.Client;
-import org.example.springbank.services.AccountService;
-import org.example.springbank.services.ClientService;
+import org.example.springbank.enums.UserRole;
 import org.example.springbank.services.DemoDataService;
+import org.example.springbank.services.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig implements WebMvcConfigurer {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AppConfig extends GlobalMethodSecurityConfiguration implements WebMvcConfigurer {
+
+    public static final String ADMIN_LOGIN = "admin";
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
@@ -21,12 +24,22 @@ public class AppConfig implements WebMvcConfigurer {
                 .addResourceLocations("/WEB-INF/static/");
     }
     @Bean
-    public CommandLineRunner demo(final DemoDataService demoDataService) {
+    public CommandLineRunner demo(final DemoDataService demoDataService,
+                                  final UserService userService,
+                                  final PasswordEncoder encoder) {
         return new CommandLineRunner() {
             @Override
             public void run(String... strings) throws Exception {
 
+                userService.addUser(ADMIN_LOGIN,
+                        encoder.encode("password"),
+                        UserRole.ADMIN, "", "", "");
+                userService.addUser("user",
+                        encoder.encode("password"),
+                        UserRole.USER, "", "", "");
+
                 demoDataService.generateDemoData();
+
             }
         };
     }
--- src/main/java/org/example/springbank/services/UserDetailsServiceImpl.java
+++ src/main/java/org/example/springbank/services/UserDetailsServiceImpl.java
@@ -1,17 +1,21 @@
 package org.example.springbank.services;
 
 import org.example.springbank.models.CustomUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
     private final UserService userService;
 
     public UserDetailsServiceImpl(UserService userService) {
@@ -21,6 +25,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
         CustomUser customUser = userService.findByLogin(login);
+
         if (customUser == null)
             throw new UsernameNotFoundException(login + " not found");
 
